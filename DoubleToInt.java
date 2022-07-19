5bdfe6f977182a6f6bfab66da91774f418a81797
Darshan1208 <108928947+Darshan1208@users.noreply.github.com>
2022-07-19 21:29:24 +0530

    Delete DoubleToInt.java

--- DoubleToInt.java
+++
@@ -1,8 +0,0 @@
-{  
-         public static void main(String args[])
-          {  
-            double d=10.5;  
-            int i=(int)d;  
-            System.out.println(i);  
-          }
-}  
